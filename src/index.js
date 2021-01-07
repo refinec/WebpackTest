739f59c5d2f6d297a281082446f3aee64a01d7e0
CrazyMKJ <c928795895@gmail.com>
2021-01-07 16:51:52 +0800

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import _ from 'lodash'
+
 function component() {
     let element = document.createElement('div');
   
-    // lodash（目前通过一个 script 引入）对于执行这一行是必需的
+    // 将 array 中的所有元素转换为由 separator 分隔的字符串
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
     return element;
---
+++ webpacj.config.js
@@ -0,0 +1,9 @@
+const path = reuqire('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
